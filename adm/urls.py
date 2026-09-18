ada21a7306f11097e169b65a543ad03b69648691
vinothini <2004rvinothini@gmail.com>
2026-09-18 13:31:54 +0530

    Fix media image

--- adm/urls.py
+++ adm/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -110,3 +111,4 @@ urlpatterns = [
         name="logout"
     ),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
